68616d29047e43e48b6dec4cd171631dacae03a6
FriyadDev <riyadhasanblue2323@gmail.com>
2024-01-22 00:25:10 +0600

    refactor(FullProject): :recycle: Server added + client and server folder separated

--- client/src/redux/store.ts
+++ client/src/redux/store.ts
@@ -1,12 +1,15 @@
 import { configureStore } from '@reduxjs/toolkit';
 import cartReducer from './features/cart/cartSlice';
 import productReducer from './features/product/productSlice';
+import { api } from './api/apiSlice';
 
 export const store = configureStore({
   reducer: {
     cart: cartReducer,
     product: productReducer,
+    [api.reducerPath]: api.reducer,
   },
+  middleware: (gDM) => gDM().concat(api.middleware),
 });
 
 export type RootState = ReturnType<typeof store.getState>;
