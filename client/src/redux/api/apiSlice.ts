067cac06f95863a649e8b7c737dc109c8cf4bf65
FriyadDev <riyadhasanblue2323@gmail.com>
2024-01-22 05:10:37 +0600

    feat(ProductCart): :sparkles: Comment API implemented to the Product Review page

--- client/src/redux/api/apiSlice.ts
+++ client/src/redux/api/apiSlice.ts
@@ -3,15 +3,36 @@ import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
 export const api = createApi({
   reducerPath: 'api',
   baseQuery: fetchBaseQuery({ baseUrl: 'http://localhost:5000' }),
-
+  tagTypes: ['comments'],
   endpoints: (builder) => ({
     getProducts: builder.query({
       query: () => '/products',
     }),
+
     getSignleProduct: builder.query({
       query: (id) => `/product/${id}`,
+    }),
+
+    postComment: builder.mutation({
+      query: ({ id, data }) => ({
+        url: `/comment/${id}`,
+        method: 'POST',
+        body: data,
+      }),
+      invalidatesTags: ['comments'],
+    }),
+
+    getComments: builder.query({
+      query: (id) => `/comment/${id}`,
+      providesTags: ['comments'],
+      keepUnusedDataFor: 10,
     }),
   }),
 });
 
-export const { useGetProductsQuery, useGetSignleProductQuery } = api;
+export const {
+  useGetProductsQuery,
+  useGetSignleProductQuery,
+  usePostCommentMutation,
+  useGetCommentsQuery,
+} = api;
