f9a1f9abdfd3c5042b89eadd4b10d39cb02f3ae0
FriyadDev <riyadhasanblue2323@gmail.com>
2024-01-22 01:21:10 +0600

    feat(ProductCart): :sparkles: Products API implemented to the api + Product Details API completed with UI implementation

--- client/src/redux/api/apiSlice.ts
+++ client/src/redux/api/apiSlice.ts
@@ -2,13 +2,16 @@ import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
 
 export const api = createApi({
   reducerPath: 'api',
-  baseQuery: fetchBaseQuery({ baseUrl: 'http://localhost:5000/products' }),
+  baseQuery: fetchBaseQuery({ baseUrl: 'http://localhost:5000' }),
 
   endpoints: (builder) => ({
     getProducts: builder.query({
       query: () => '/products',
     }),
+    getSignleProduct: builder.query({
+      query: (id) => `/product/${id}`,
+    }),
   }),
 });
 
-export const { useGetProductsQuery, useLazyGetProductsQuery } = api;
+export const { useGetProductsQuery, useGetSignleProductQuery } = api;
